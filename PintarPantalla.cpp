b52bc8e2cc33931cac4ea404fd614c3852b4a011
Alex <alex3dartgames@gmail.com>
2020-03-25 15:52:58 +0100

    Add project files.

---
+++ PintarPantalla.cpp
@@ -0,0 +1,12 @@
+﻿// PintarPantalla.cpp : Defines the entry point for the application.
+//
+
+#include "PintarPantalla.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
